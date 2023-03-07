2ea76c0fa03d1ddf73253a79bfbf166c1c8b0348
Sushil Jaiswal <sushil.jaiswal@oracle.com>
2023-03-08 00:45:49 +0530

    Introducing bootstrap to react app.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>We made our first React application!</p>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Code your project</td>
+                <td>Sushil</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Get hairCut</td>
+                <td>Sushil</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
