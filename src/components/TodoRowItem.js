d314f2f3dc726444e2a134cb90263db9593b1a43
Sushil Jaiswal <sushil.jaiswal@oracle.com>
2023-03-08 00:49:36 +0530

    Introducing First Component based rendering for a table row

---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,11 @@
+function TodoRowItem() {
+    return (
+        <tr>
+            <th scope='row'>1</th>
+            <td>Code your project</td>
+            <td>Sushil</td>
+        </tr>
+    );
+}
+
+export default TodoRowItem;
