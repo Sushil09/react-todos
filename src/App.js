ed582f8e2f031aea5ea055c153cf8b8a872ff4dd
Sushil Jaiswal <sushil.jaiswal@oracle.com>
2023-03-09 00:30:02 +0530

    Dynamically create components based on number of items within the array.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoRowItem from './components/TodoRowItem';
 import TodoTable from './components/TodoTable';
 
 
@@ -8,9 +6,12 @@ import TodoTable from './components/TodoTable';
 
 function App() {
 
-  const todos = [{ rowNumber: 1, rowDescription: "Code your project", rowAssigned: "User 1" },
+  const todos = [
+  { rowNumber: 1, rowDescription: "Code your project", rowAssigned: "User 1" },
   { rowNumber: 2, rowDescription: "Get Haircut", rowAssigned: "User 2" },
-  { rowNumber: 3, rowDescription: "Sleep", rowAssigned: "User 1" }];
+  { rowNumber: 3, rowDescription: "Sleep", rowAssigned: "User 1" },
+  { rowNumber: 4, rowDescription: "Charge Laptop", rowAssigned: "User 2" }
+];
 
   return (
     <div className='mt-5 container'>
